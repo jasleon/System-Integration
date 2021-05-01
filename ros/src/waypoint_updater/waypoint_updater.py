94c95bcc9d83df10ac9b5af2bef550263e5f678c
Antonio Sanchez <antonio.buap@gmail.com>
2021-05-01 14:39:44 -0500

    feat: Adjust values to improve simulation performance

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -51,7 +51,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(25)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 self.publish_waypoints()
